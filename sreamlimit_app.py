960cd26cec764e0229bc237bbb09189c62a81205
conydre2020 <125288603+conydre2020@users.noreply.github.com>
2023-02-13 14:57:08 -0600

    Update sreamlimit_app.py
    typo was fixed

--- sreamlimit_app.py
+++ sreamlimit_app.py
@@ -3,7 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.Header ('Breakfast Menu')
-
 streamlit.text ('omega 3 Blueberry Oatmeal')
 streamlit.text('kale, spinach & Rocket Smoothiew')
 streamlit.text('Hard-Boiled Free-Range Egg')
