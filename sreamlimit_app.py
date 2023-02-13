10b2ce4e3a41421f85c2954a11cb66b4132ca844
conydre2020 <125288603+conydre2020@users.noreply.github.com>
2023-02-13 14:52:33 -0600

    Update sreamlimit_app.py

--- sreamlimit_app.py
+++ sreamlimit_app.py
@@ -2,7 +2,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.Header ('Breakfast Menu")
+streamlit.Header ('Breakfast Menu')
 
 streamlit.text ('omega 3 Blueberry Oatmeal')
 streamlit.text('kale, spinach & Rocket Smoothiew')
