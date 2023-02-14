734ccfda792f3794a079c31da89ff390d8c563ea
conydre2020 <125288603+conydre2020@users.noreply.github.com>
2023-02-14 07:59:03 -0600

    Update sreamlimit_app.py

--- sreamlimit_app.py
+++ sreamlimit_app.py
@@ -22,7 +22,7 @@ streamlit.dataframe(my_fruit_list)
 streamlit.multiselect ("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
  #lets put a pick list here so they can pick the fruit they want to include
   
-  fruits_select = streamlit.multiselect ("pick some fruits:", list (my_fruit_list.index),['Avocado','Strawberries'])
+  #fruits_select = streamlit.multiselect ("pick some fruits:", list (my_fruit_list.index),['Avocado','Strawberries'])
   fruits_to_show = my_fruit_list.loc [fruits_select ]
   #display the table on the page
   streamlit.datafram(fruits_to_show)
