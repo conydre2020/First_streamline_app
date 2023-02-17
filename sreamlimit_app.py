87aab0eb1ff5bc685a2f263aa4a2e31353239611
conydre2020 <125288603+conydre2020@users.noreply.github.com>
2023-02-17 09:24:23 -0600

    Update sreamlimit_app.py

--- sreamlimit_app.py
+++ sreamlimit_app.py
@@ -3,3 +3,7 @@ streamlit.header ('Fruityvice FruitAdvice')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json ())
+
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
