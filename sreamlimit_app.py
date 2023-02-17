7558dfb8560bffc6bb1e1ba5ef9676cc7787b008
conydre2020 <125288603+conydre2020@users.noreply.github.com>
2023-02-17 09:06:49 -0600

    Update sreamlimit_app.py

--- sreamlimit_app.py
+++ sreamlimit_app.py
@@ -1,28 +1,4 @@
 
-import streamlit
-streamlit.title('My Parents New Healthy Diner')
-
-streamlit.header('Breakfast Menu')
-streamlit.text ('omega 3 Blueberry Oatmeal')
-streamlit.text('kale, spinach & Rocket Smoothiew')
-streamlit.text('Hard-Boiled Free-Range Egg')
-
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
-
-# let's put a pick list here so they can pick the fruit they want to include
-
-streamlit.multiselect ("pick some fruits:", list (my_fruit_list.index))
-
-#display the table on the page
-streamlit.dataframe(my_fruit_list)
-
-#let's put a pick list here so they can pick the fruit they want to include
-streamlit.multiselect ("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
- #lets put a pick list here so they can pick the fruit they want to include
-fruits_select = streamlit.multiselect ("pick some fruits:", list (my_fruit_list.index),['Avocado','Strawberries'])
-  
-fruits_to_show = my_fruit_list.loc [fruits_select ]
-  #display the table on the page
-streamlit.datafram(fruits_to_show)
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
