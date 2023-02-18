2a355303fb9ff096915b406ded7afba526eb4015
conydre2020 <125288603+conydre2020@users.noreply.github.com>
2023-02-18 06:41:42 -0600

    Update sreamlimit_app.py

--- sreamlimit_app.py
+++ sreamlimit_app.py
@@ -9,3 +9,5 @@ streamlit.text(fruityvice_response.json ())
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+
+import snowflake.connector 
